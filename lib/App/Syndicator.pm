b567ee4406c9b62caadc1050b27a943fa381f188
Robin Edwards <robin.ge@gmail.com>
2010-06-25 00:07:37 +0100

    updated docs

--- lib/App/Syndicator.pm
+++ lib/App/Syndicator.pm
@@ -12,7 +12,6 @@ class App::Syndicator with (App::Syndicator::Config,
     use MooseX::Types::URI 'Uri';
     use MooseX::Types -declare=>[qw/UriArray/];
     use MooseX::Types::Moose qw/ArrayRef Str/;
-    use Data::Dumper;
     
     subtype UriArray,
         as ArrayRef[Uri];
@@ -110,53 +109,33 @@ class App::Syndicator with (App::Syndicator::Config,
 
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
 
 =head1 NAME
 
-App::Syndicator - Perl extension for blah blah blah
+App::Syndicator - Perl application for feed syndicationh
 
 =head1 SYNOPSIS
 
-  use App::Syndicator;
-  blah blah blah
+ $ syndicator | less -r
 
 =head1 DESCRIPTION
 
-Stub documentation for App::Syndicator, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
-
-=head2 EXPORT
-
-None by default.
-
-
+...
 
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
+...
 
 =head1 AUTHOR
 
-Rob Edwards, E<lt>robin.ge@gmail.comE<gt>
+Rob Edwards, E<lt>rge@cpan.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
 Copyright (C) 2010 by Rob Edwards
 
 This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.8.8 or,
+it under the same terms as Perl itself, either Perl version 5.12.1 or,
 at your option, any later version of Perl 5 you may have available.
-
 
 =cut
